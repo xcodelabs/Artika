7a674872500535be1eb25bd64197dccffe83157d
Sabari-nath-p <sabarinath5604@gmail.com>
2022-12-05 19:03:54 +0530

    project created

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+void main(List<String> args) {
+  runApp(Artica());
+}
+
+class Artica extends StatelessWidget {
+  const Artica({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
